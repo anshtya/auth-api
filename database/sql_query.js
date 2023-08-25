8972b5ba0f0bb98291c6b24bc3249a1bd1038da2
Ansh Tyagi <tyagiansh486@gmail.com>
2023-08-25 15:10:26 +0530

    add bcrypt and status codes

--- database/sql_query.js
+++ database/sql_query.js
@@ -1,11 +1,26 @@
-const userTableQuery = 'CREATE TABLE IF NOT EXISTS users (user_email VARCHAR, user_password CHAR(8));';
+const userTableQuery = `CREATE TABLE IF NOT EXISTS users (
+    user_id BIGSERIAL PRIMARY KEY,
+    user_name VARCHAR(15) UNIQUE NOT NULL,
+    user_email VARCHAR UNIQUE NOT NULL,
+    user_password VARCHAR NOT NULL,
+    user_bio VARCHAR(150) DEFAULT '' NOT NULL
+);`;
 
-const userInsertQuery = (email, password) => {
-    return `INSERT INTO users VALUES ('${email}', '${password}');`
+const userInsertQuery = (username, email, password) => {
+    return `INSERT INTO users (user_name, user_email, user_password) 
+    VALUES ('${username}', '${email}', '${password}');`
+};
+
+const userCheckQuery = (email, username) => {
+    return `SELECT FROM users WHERE user_email = '${email}' OR user_name = '${username}';`
+};
+
+const userGetHashQuery = (email, username) => {
+    return `SELECT user_password FROM users WHERE user_email = '${email}' OR user_name = '${username}';`
+};
+
+const userGetUserName = (username) => {
+    return `SELECT user_name FROM users WHERE user_name = '${username}';`
 }
 
-const userCheckQuery = (email) => {
-    return `SELECT FROM users WHERE user_email = '${email}';`
-}
-
-module.exports = { userTableQuery, userInsertQuery, userCheckQuery };
+module.exports = { userTableQuery, userInsertQuery, userCheckQuery, userGetHashQuery, userGetUserName };
--- routes/auth.js
+++ routes/auth.js
@@ -2,36 +2,82 @@ const express = require('express');
 const router = express.Router();
 const dotenv = require('dotenv');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const { pool } = require('../database/db');
-const { userInsertQuery, userCheckQuery } = require('../database/sql_query');
+const { userInsertQuery, userCheckQuery, userGetHashQuery, userGetUserName } = require('../database/sql_query');
 
 dotenv.config();
 
 router.post('/signup', async (req, res) => {
-    const { email, password } = req.body;
-
     try {
-        await pool.query(userInsertQuery(email, password));
-        const token = jwt.sign({ user_email: email }, process.env.ACCESS_TOKEN, { expiresIn: '15m' });
-        res.json({ access_token: token });
-    } catch (err) {
-        res.json({ "error": `${err.message}` });
+        const { username, email, password } = req.body;
+
+        const result = await pool.query(userCheckQuery(email, username));
+        const rowCount = result.rowCount;
+
+        if (rowCount != 0) {
+            res.status(400).json({ "error": "User already exists. Please sign-in instead" });
+        } else {
+            const hash = await bcrypt.hash(password, 10);
+
+            await pool.query(userInsertQuery(username, email, hash));
+            const token = jwt.sign({
+                user_email: email,
+                username: username
+            }, process.env.ACCESS_TOKEN, { expiresIn: '15m' });
+
+            res.status(201).json({ "access_token": `${token}` });
+        }
+    } catch (error) {
+        console.log("error: ", error)
+        res.status(500).json({ "error": "An error occured" });
     }
 });
-router.post('/signin', async (req, res) => {
-    const { email, password } = req.body;
 
+router.post('/signin', async (req, res) => {
     try {
-        const result = await pool.query(userCheckQuery(email, password));
+        const { username, email, password } = req.body;
+
+        const result = await pool.query(userCheckQuery(email, username));
         const rowCount = result.rowCount;
         if (rowCount == 0) {
-            res.json({ "error": "User doesn't exist. Please sign-up" });
+            res.status(401).json({ "error": "User doesn't exist. Please sign-up" });
         } else {
-            const token = jwt.sign({ user_email: email }, process.env.ACCESS_TOKEN, { expiresIn: '15m' });
-            res.json({ "access_token": `${token}` });
+            const result = await pool.query(userGetHashQuery(email, username));
+            const hash = result.rows[0].user_password;
+            const isPasswordValid = await bcrypt.compare(password, hash);
+
+            if (isPasswordValid) {
+                const token = jwt.sign({
+                    user_email: email,
+                    username: username
+                }, process.env.ACCESS_TOKEN, { expiresIn: '15m' });
+
+                res.status(200).json({ "access_token": `${token}` });
+            } else {
+                res.status(401).json({ "error": "Incorrect password" });
+            }
         }
-    } catch (err) {
-        res.json({ "error": `${err.message}` });
+    } catch (error) {
+        console.log("error: ", error)
+        res.status(500).json({ "error": "An error occured" });
+    }
+});
+
+router.post('/checkusername', async (req, res) => {
+    try {
+        const { username } = req.body;
+        const result = await pool.query(userGetUserName(username));
+        const rowCount = result.rowCount;
+
+        if (rowCount != 0) {
+            res.status(400).json({ "error": "Username already exists" });
+        } else {
+            res.status(200).json({ "message": "OK" });
+        }
+    } catch (error) {
+        console.log("error: ", error)
+        res.status(500).json({ "error": "An error occured" });
     }
 });
 
